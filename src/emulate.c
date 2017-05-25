de91dd1c81c5dd001b8f39d99be2a487d71e3f47
Laurence Squires <ljs116@ray25.doc.ic.ac.uk>
2017-05-25 14:12:27 +0100

    Argument loading working

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,15 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
+  if(argc != 2) {
+    fprintf(stderr, "Incorrect number of arguments provided.\n");
+    return EXIT_FAILURE;
+  }
+
+  char *filename = argv[1];
+  printf("%s\n", filename);
+
+
   return EXIT_SUCCESS;
 }
