c1542cf8282f8f9381f61d62bb14e3f5dca6f744
Qiang Feng <Qiang.Feng@hotmail.co.uk>
2017-05-25 14:13:47 +0100

    Fixed code style

--- src/emulate.c
+++ src/emulate.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  if(argc != 2) {
+  if (argc != 2) {
     fprintf(stderr, "Incorrect number of arguments provided.\n");
     return EXIT_FAILURE;
   }
