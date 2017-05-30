6d0b2a5e11ba4e34a162e0a43ac0cc089c27b2e4
Qiang Feng <Qiang.Feng@hotmail.co.uk>
2017-05-30 17:43:34 +0100

    Fixed prototype type definition for shifter

--- src/toolbox.h
+++ src/toolbox.h
@@ -4,7 +4,7 @@
 #include "global.h"
 #include "system_state.h"
 
-void exit_program();
+void exit_program(system_state_t *machine);
 void print_array(void *p, size_t bytes_to_print);
 word_t get_word(system_state_t *machine, address_t mem_address);
 word_t negate(word_t value);
@@ -15,6 +15,6 @@ void print_registers(system_state_t *machine);
 void print_memory(system_state_t *machine);
 long twos_complement_to_long(word_t value);
 void print_binary_value(word_t value);
-word_t shifter(shift_t type, word_t shift_amount, word_t value);
+value_carry_t shifter(shift_t type, word_t shift_amount, word_t value);
 
 #endif
