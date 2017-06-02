ed4331bade7c32c409321fc8a5712b73cb1eac4d
Alexander Clarke <ac3716@point45.doc.ic.ac.uk>
2017-06-02 14:07:43 +0100

    Fixed encode

--- src/encode.c
+++ src/encode.c
@@ -40,7 +40,7 @@ word_t encode_dpi(instruction_t *instruction) {
   }
   if (instruction->flag_0) {
     binary |= instruction->immediate_value;
-    binary |= instruction->shift_amount << 8;
+    binary |= (0xF & instruction->shift_amount) << 8;
   } else {
     binary |= instruction->rm;
     binary |= ((word_t) instruction->shift_type) << 5;
