28f2e55509684fd220f596989f680cf38904f0fa
Jordan Spooner <js4416@ic.ac.uk>
2017-06-03 15:44:41 +0100

    Fixed GPIO program, works on RPi

--- programs/gpio.s
+++ programs/gpio.s
@@ -1,19 +1,19 @@
 ldr r0,=0x20200000
-mov r1,#18
+mov r1,#0x40000
+mov r3,#0x10000
 str r1,[r0,#4]
-str r1,[r0,#0x28]
+str r3,[r0,#0x28]
 loop:
 ldr r2,=0xFFFFFF
 wait1:
 sub r2,r2,#1
 cmp r2,#0
 bne wait1
-str r1,[r0,#0x1c]
+str r3,[r0,#0x1c]
 ldr r2,=0xFFFFFF
 wait:
 sub r2,r2,#1
 cmp r2,#0
 bne wait
-str r1,[r0,#0x28]
+str r3,[r0,#0x28]
 b loop
-andeq r0,r0,r0
