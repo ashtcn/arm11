d12d6bb6b404bd7445a64adfe03595e25a2575f7
Alexander Clarke <ac3716@point45.doc.ic.ac.uk>
2017-06-13 15:48:17 +0100

    Added comments to word array

--- src/assemble_utils/word_array.c
+++ src/assemble_utils/word_array.c
@@ -1,5 +1,17 @@
+/**
+ * @file word_array.c
+ * @brief Functions for using word arrays.
+ */
 #include "word_array.h"
 
+/**
+ * @brief Initialises a word array
+ *
+ * Mallocs the memory for the array, exits the program if that was not possible.
+ * It initialises the size of the array to INITIAL_ARRAY_SIZE, a #define in
+ * global.h.
+ * @return An initinalised word array.
+ */
 word_array_t *make_word_array(void) {
   word_array_t *word_array = malloc(sizeof(word_array_t));
   if (!word_array) {
@@ -16,6 +28,13 @@ word_array_t *make_word_array(void) {
   return word_array;
 }
 
+/**
+ * @brief Adds a word to a word array
+ *
+ * If more memory is required it doubles the array size.
+ * @param word_array The word array to add the word to.
+ * @param word The word to add to the array.
+ */
 void add_word_array(word_array_t *word_array, word_t word) {
   if (word_array->size >= word_array->max_elements) {
     word_array->max_elements *= 2;
@@ -29,6 +48,11 @@ void add_word_array(word_array_t *word_array, word_t word) {
   word_array->size++;
 }
 
+/**
+ * @brief Frees all memory used in a word array.
+ *
+ * @param word_array The word array to free.
+ */
 void free_word_array(word_array_t *word_array) {
   free(word_array->array);
   free(word_array);
